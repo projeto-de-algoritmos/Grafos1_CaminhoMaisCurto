a1e1e924c3763b768c7334600e185e9fd9044be5
Sebastian Zuzunaga <seb55381@hotmail.com>
2023-09-16 17:56:15 -0500

    Added adjacency list

--- CidadesCaminho.py
+++ CidadesCaminho.py
@@ -1,1 +1,36 @@
-##Nothin yet just testing
+# Crear lista de adjacencia
+# Lista usa texto en lugar de numeros
+
+class Grafo:
+
+    def __init__(self):
+        self.grafo = {}
+
+    def adiciona_nodo(self, nodo):
+        if nodo not in self.grafo:
+            self.grafo[nodo] = []
+
+    def adiciona_aresta(self, u, v):
+        self.grafo[u].append(v)
+        self.grafo[v].append(u)
+
+    def mostra_lista(self):
+        for nodo, vecinos in self.grafo.items():
+            print(f'{nodo}:', end ='  ')
+            for vecino in vecinos:
+                print(f'{vecino}  ->', end='  ')
+            print('')
+
+g = Grafo()
+
+g.adiciona_nodo("Brasilia")
+g.adiciona_nodo("Gamma")
+g.adiciona_nodo("Floreanopolis")
+g.adiciona_nodo("Aguas claras")
+
+g.adiciona_aresta("Brasilia", "Gamma")
+g.adiciona_aresta("Brasilia", "Floreanopolis")
+g.adiciona_aresta("Floreanopolis", "Aguas claras")
+g.adiciona_aresta("Aguas claras", "Brasilia")
+
+g.mostra_lista()
