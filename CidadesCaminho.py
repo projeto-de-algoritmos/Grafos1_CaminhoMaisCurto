c71d5c540ad746befa2e457117bfeac42cfc40dd
Sebastian Zuzunaga <seb55381@hotmail.com>
2023-09-25 12:25:53 -0500

    Some final details added

--- CidadesCaminho.py
+++ CidadesCaminho.py
@@ -7,6 +7,17 @@ class Grafo:
         if nodo not in self.grafo:
             self.grafo[nodo] = []
 
+    def deletar_nodo(self, nodo):
+        for vecino in self.grafo[nodo]:
+            self.grafo[vecino].remove(nodo)
+        del self.grafo[nodo]
+
+    def verificar_nodo(self,nodo):
+        if nodo not in self.grafo:
+            return 1
+        else:
+            return 0
+
     def adiciona_aresta(self, u, v):
         self.grafo[u].append(v)
         self.grafo[v].append(u)
@@ -53,38 +64,57 @@ g.adiciona_aresta("Brasilia", "Floreanopolis")
 g.adiciona_aresta("Floreanopolis", "Aguas claras")
 g.adiciona_aresta("Aguas claras", "Brasilia")
 
-g.mostra_lista()
-
-camino_mas_corto = g.bfs("Gamma","Aguas claras")
-
-if camino_mas_corto and camino_mas_corto != -1:
-    print(f'El camino más corto es: {camino_mas_corto}')
-elif camino_mas_corto == -1:
-    print(f'Ya estas aqui')
-else:
-    print('No hay camino entre los nodos.')
-
-option = 0
-inicio = 0
-final = 0
-nodo = 0
-nodo_con = 0
-while option != 5:
+while True:
     print('######Bemvindo a WayFinder#######')
-    print('Seleccione una opcion')
-    print('1.- Inserir nodo')
-    print('2.- Conectar nodo')
-    print('3.- Verificar estado de nodos')
-    print('4.- Buscar camino mas corto')
-    print('5.- Salir')
-
+    print('Selecione uma opção')
+    print('1.- Inserir uma cidade')
+    print('2.- Conectar uma cidade')
+    print('3.- Apagar uma cidade')
+    print('4.- Verificar o estado das cidades')
+    print('5.- Encontrar caminho mais curto')
+    print('6.- Sair')
     option = input()
 
-    match option:
-        case 1:
-            nodo = input('Ingrese nombre del nodo: ')
+    if option == '1':
+        nodo = input('Digite o nome da cidade: ')
+        compin = g.verificar_nodo(nodo)
+        if compin == 0:
             g.adiciona_nodo(nodo)
-        case 2:
-            nodo = input('Ingrese nombre del nodo a conectar: ')
-            nodo_con = input('Ingrese nombre del nodo con el cual hacer la conexion')
+        else:
+            print('A cidade já está registrada')
+    elif option == '2':
+        nodo = input('Digite o nome da cidade para conectar: ')
+        nodo_con = input('Digite o nome da cidade para fazer a conexão: ')
+        comp1 = g.verificar_nodo(nodo)
+        comp2 = g.verificar_nodo(nodo_con)
+        if comp1 == 1 or comp2 == 1:
+            print('Alguma das cidades não existem')
+        else:
             g.adiciona_aresta(nodo,nodo_con)
+    elif option == '3':
+        nodod = input('Digite o nome da cidade para apagar: ')
+        compdel = g.verificar_nodo(nodod)
+        if compdel == 1:
+            print('Cidade não existe')
+        else:
+            g.deletar_nodo(nodod)
+            print('A cidade foi apagada')
+    elif option == '4':
+        g.mostra_lista()
+    elif option == '5':
+        inicio = input('Coloque o nome da cidade de partida: ')
+        final = input('Coloque o nome da cidade de destino: ')
+        compinit = g.verificar_nodo(inicio)
+        compfinal = g.verificar_nodo(final)
+        if compinit == 1 or compfinal == 1:
+            print('Algumaa das cidades não existem')
+        else:
+            camino_mas_corto = g.bfs(inicio, final)
+            if camino_mas_corto and camino_mas_corto != -1:
+                print(f'O caminho mais curto é: {camino_mas_corto}')
+            elif camino_mas_corto == -1:
+                print(f'Você já está aqui')
+            else:
+                print('Não há caminho entre as cidades.')
+    elif option == '6':
+        break
